0eda6e743d984777fbff0db7ca49bc91520c041e
Eri <eriosvaldo.henrique369@gmail.com>
2026-03-01 17:32:41 -0300

    Coisas de leve

---
+++ src/relatorios.js
@@ -0,0 +1,30 @@
+import Chart from 'chart.js/auto'
+
+(async function() {
+  const data = [
+    { categoria: "Alimentação", count: 860 },
+    { categoria: "Aluguel", count: 1700 },
+    { categoria: "Transportes", count: 265 },
+    { categoria: "Lazer", count: 2400 },
+    { categoria: "Conta de Água", count: 150 },
+    { categoria: "Conta de Luz", count: 300 },
+  ];
+
+  new Chart(
+    document.getElementById('relatorios'),
+    {
+      type: "doughnut",
+      data: {
+        labels: data.map(row => row.categoria),
+        datasets: [
+          {
+            label: 'Valor',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
+
+console.log(Chart)
